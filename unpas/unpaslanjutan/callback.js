ff1cdb4c7a15282c0e2a7ca83c61651e5358af21
Veronica Maya Santi <veronicamayasanti@gmail.com>
2022-11-16 23:59:14 +0700

    call back

--- unpas/unpaslanjutan/callback.js
+++ unpas/unpaslanjutan/callback.js
@@ -25,36 +25,50 @@
 
 
 
-const mhs = [
-    {
-        "nama": "veronica maya",
-        "nrp": "010592",
-        "email": "vero@gmail.com",
-        "jurusan": "pemat",
-        "idDosenWali": 1
-    },
-    {
-        "nama": "vsanti wijaya",
-        "nrp": "0920501",
-        "email": "santi@gmail.com",
-        "jurusan": "pefis",
-        "idDosenWali": 2
-    },
-    {
-        "nama": "samantha",
-        "nrp": "281217",
-        "email": "samantha@gmail.com",
-        "jurusan": "pebio",
-        "idDosenWali": 3
-    },
-    {
-        "nama": "geonathan",
-        "nrp": "141120",
-        "email": "geo@gmail.com",
-        "jurusan": "pgsd",
-        "idDosenWali": 4
-    },
-]
+
+// contoh 3. synchronous callback
+// const mhs = [
+//     {
+//         "nama": "veronica maya",
+//         "nrp": "010592",
+//         "email": "vero@gmail.com",
+//         "jurusan": "pemat",
+//         "idDosenWali": 1
+//     },
+//     {
+//         "nama": "vsanti wijaya",
+//         "nrp": "0920501",
+//         "email": "santi@gmail.com",
+//         "jurusan": "pefis",
+//         "idDosenWali": 2
+//     },
+//     {
+//         "nama": "samantha",
+//         "nrp": "281217",
+//         "email": "samantha@gmail.com",
+//         "jurusan": "pebio",
+//         "idDosenWali": 3
+//     },
+//     {
+//         "nama": "geonathan",
+//         "nrp": "141120",
+//         "email": "geo@gmail.com",
+//         "jurusan": "pgsd",
+//         "idDosenWali": 4
+//     },
+// ]
+
+// console.log('mulai');
+
+// mhs.forEach(m => {
+//     for (let i=0; i < 10000000; i++){
+//         let date = new Date();
+//     }
+//     console.log(m.nama)
+// });
+
+// console.log('selesai');
 
 
-mhs.forEach(m => console.log(m.nama));
+// contoh 4. Asynchronous callback
+//

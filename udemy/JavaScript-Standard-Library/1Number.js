25aa9b7650c5ec3f154a16a52fdf3e9a8b0bb4b7
Veronica Maya Santi <veronicamayasanti@gmail.com>
2022-12-22 20:53:31 +0700

    string

--- udemy/JavaScript-Standard-Library/1Number.js
+++ udemy/JavaScript-Standard-Library/1Number.js
@@ -30,3 +30,21 @@ console.log(Number.NaN);
 // Number.isNaN(value) untuk mengecek apakah value NaN atau bukan
 // Number.isInteger(value) untuk mengecek apakah value berupa integer atau bukan
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number#static_methods 
+
+const data = Number('12345');
+
+console.log(Number.isInteger(data));
+console.log(Number.isNaN(data));
+
+
+// Number Instance Method
+// Number memiliki banyak instance method, seperti :
+// Number.toLocalString(locale) untuk mengubah number menjadi string sesuai locale: https://www.lansweeper.com/knowledgebase/list-of-currency-culture-codes/ 
+// Number.toString(radix) untuk mengubah number menjadi string sesuai radix
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number#instance_methods 
+
+
+const value = Number(12345);
+console.log(value.toString(2));
+console.log(value.toLocaleString("id-ID"));
+
---
+++ udemy/JavaScript-Standard-Library/2String.js
@@ -0,0 +1,20 @@
+// Tipe data String sudah kita bahas pada materi JavaScript Dasar
+// Namun kita belum membahas instance method atau juga instance properties yang terdapat di String
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String 
+
+
+// String Instance Method dan Properties
+// String sendiri memiliki banyak sekali instance method dan properties
+// Hal ini menjadikan untuk memanipulasi data String sangat mudah di JavaScript, seperti mengubah menjadi lowercase, UPPERCASE, memotong string menjadi array dan lain - lain
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#instance_methods
+
+
+const name = "veronica maya santi"
+
+console.log(name.length);
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+console.log(name.split(" "));
+
+
+

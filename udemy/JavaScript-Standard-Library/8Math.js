874363f0ca64154ca6a1d7bbe4f41212e36537be
Veronica Maya Santi <veronicamayasanti@gmail.com>
2023-01-03 20:17:36 +0700

    Math

---
+++ udemy/JavaScript-Standard-Library/8Math.js
@@ -0,0 +1,12 @@
+// math merupakan class di JavaScript yang berisikan static property dan method untuk operaso matematika
+//  Math hanya bisa digunakan untuk tipe data Number, tidak bisa digunakan untuk tipa data BigInt
+
+console.log(Math.max(12,12,13,14,15));
+console.log(Math.min(12, 12, 13, 14, 15));
+console.log(Math.round(10.5));
+console.log(Math.round(13.3));
+console.log(Math.floor(8.4));
+console.log(Math.ceil(10.3));
+
+
+

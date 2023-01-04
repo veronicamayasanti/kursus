33ce7261e2c56b152bbc684e5e0fb7390f1392b4
Veronica Maya Santi <veronicamayasanti@gmail.com>
2023-01-04 19:40:42 +0700

    Boolean

---
+++ udemy/JavaScript-Standard-Library/9Boolean.js
@@ -0,0 +1,10 @@
+// Boolean merupaka wrapper class untuk tipe primitif boolean
+//  Boolean memiliki method toString() untuk mengkonversi ke String
+//  dan memilikik method valueOf() untuk mengkonversi ke tipe biilean primitif
+
+const boolean = Boolean(true);
+
+console.log(boolean);
+console.log(typeof boolean);
+console.log(boolean.toString());
+console.log(boolean.valueOf());

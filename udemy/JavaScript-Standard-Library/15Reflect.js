ce6fce0d7a93cbd6034247ce8fb9025a25c97db8
Veronica Maya Santi <veronicamayasanti@gmail.com>
2023-01-09 23:03:38 +0700

    Reflect

---
+++ udemy/JavaScript-Standard-Library/15Reflect.js
@@ -0,0 +1,3 @@
+// Reflect merupakan class yang digunakan untuk mengeksekusi JavaScript function
+// Reflect tidak memiliki constructor, dan cara penggunaan Reflect tidak dengan membuat object dengan new Reflect
+// penggunaan Reflect adalah menggunakan banyak sekali static methodnya

b702e5c0e6af0924dced9f98434f10213f2fd718
Veronica Maya Santi <veronicamayasanti@gmail.com>
2023-01-11 00:58:44 +0700

    Eval

---
+++ udemy/JavaScript-Standard-Library/18Eval.js
@@ -0,0 +1,6 @@
+// eval merupakan function yang digunakan untuk mengeksekusi kode JavaScript dari String
+// jika salah penggunaan maka mengeksekusi kode program yang bisa menyebabkan masalah keamanan di website kita
+
+const script = "alert('anda di heck')"
+
+eval(script)

41565566e0392f2fbe9f67ab0b7650c0aa3dca26
Veronica Maya Santi <veronicamayasanti@gmail.com>
2023-01-12 21:43:55 +0700

    Pengenalan JavaScript  Modules

---
+++ udemy/JavaScript-Modules/1.Pengenalan-JavaScript -Modules.js
@@ -0,0 +1,16 @@
+// // Latar Belakang
+// Dahulu kode program JavaScript biasanya dimulai dari kode yang kecil dan sedikit disetiap halaman, jadi jarang sekali kita akan menggunakan kode JavaScript dengan ukuran besar dalam satu halaman web
+// Namun saat ini, kebutuhan web sudah berbeda, semenjak makin populernya framework - framework JavaScript untuk membuat FrontEnd, kebutuhkan pembuatan kode JavaScript semakin kompleks dan besar
+// Oleh karena itu, sudah seharusnya saat ini, saat membuat kode JavaScript, kita perlu melakukan mekanisme memotong - motong kode JavaScript, sehingga tidak terlalu kompleks dan banyak dalam satu file JavaScript
+
+
+// JavaScript Modules
+// JavaScript Modules merupakan fitur dimana kita bisa membuat module(kode JavaScript yang bisa digunakan ulang) untuk kode JavaScript lain
+// JavaScript Modules diawali pada teknologi bernama NodeJS, selanjutnya banyak library yang mengadopsi penggunaan JavaScript Modules di NodeJS agar bisa berjalan di Web Browser, seperti library CommonJS, AMD, RequireJS, dan lain - lain
+// Namun untungnya, saat ini Browser modern sudah banyak mendukung JavaScript Modules, kecuali jika masih menggunakan Internet Explorer
+
+
+// Import dan Export
+// Secara garis besar, ada dua kata kunci yang digunakan dalam JavaScript Modules, yaitu import dan export
+// import merupakan kata kunci yang digunakan untuk mengambil sesuatu dari module lain
+// Sedangkan export merupakan kata kunci yang digunakan untuk memberitahu module lain, bagian mana yang ingin kita ekspose keluar, sehingga bisa di -import oleh module lain

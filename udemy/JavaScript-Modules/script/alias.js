7622be3d4a26d89bdaa89f2823c45359e507677d
Veronica Maya Santi <veronicamayasanti@gmail.com>
2023-01-14 10:01:11 +0700

    alias

---
+++ udemy/JavaScript-Modules/script/alias.js
@@ -0,0 +1,14 @@
+const company = "Maya Wijaya Company";
+
+function sum(first, second) {
+    return first * second;
+}
+
+class Company {
+
+}
+// alias di export
+// export { company as perusahaan, sum as total, Company as Perusahaan }
+
+// alias di import
+ export { company , sum , Company  }

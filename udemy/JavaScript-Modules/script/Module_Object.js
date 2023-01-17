c7254a185b4cad9c0729ceb9f64953a78c1fd493
Veronica Maya Santi <veronicamayasanti@gmail.com>
2023-01-17 09:18:37 +0700

    Module_Object

---
+++ udemy/JavaScript-Modules/script/Module_Object.js
@@ -0,0 +1,15 @@
+export const firstName = "veronica";
+export const middleName = "Maya";
+export const lastName = "Wijaya";
+
+export function hello() {
+    console.log(`hello from veronica`);
+    
+}
+
+export class Person {
+    constructor() {
+        this.name = "veronica"
+    }
+}
+

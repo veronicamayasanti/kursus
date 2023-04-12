856334f3c572a000099ff67aad6a84aa8142ab7d
veronica maya santi <veronicamayasanti@gmail.com>
2023-04-12 12:46:47 +0700

    Update Dependency

--- NODEJS/NPM/hello.js
+++ NODEJS/NPM/hello.js
@@ -1,6 +1,8 @@
 import {sayHello, sum} from "mayabelajar-nodejs-npm-library";
+import {max, min} from "mayabelajar-nodejs-npm-library"
 
 console.info(sayHello("maya"));
 
 const numbers = [10,10,10,10,10];
 console.info(sum(numbers));
+

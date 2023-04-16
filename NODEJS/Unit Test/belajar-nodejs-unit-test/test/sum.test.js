2f7bebc42ca02bda47aa5da7ba405a6cec9d8b7c
veronica maya santi <veronicamayasanti@gmail.com>
2023-04-16 19:08:09 +0700

    Equals Matchers

--- NODEJS/Unit Test/belajar-nodejs-unit-test/test/sum.test.js
+++ NODEJS/Unit Test/belajar-nodejs-unit-test/test/sum.test.js
@@ -1,7 +1,17 @@
 import {sum} from "../src/sum.js";
 
-test("test sum function",  () => {
+test("test sum function 1",  () => {
     const  result = sum(1,2);
     expect(result).toBe(3);
 });
 
+
+test("test sum function 2",  () => {
+    const  result = sum(1,2);
+    expect(result).toBe(3);
+});
+
+test("test sum function 3",  () => {
+    const  result = sum(1,2);
+    expect(result).toBe(3);
+});
